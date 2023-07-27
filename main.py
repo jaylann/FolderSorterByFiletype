1e97f64da1d9c43b9d794b374086820a2c48cbbe
Jaylann <Justin.Lanfermann@hotmail.com>
2023-07-27 21:57:38 +0200

    Enhanced file and folder sorting with improved error handling and command line interface
    This commit introduces significant improvements to the file and folder sorting functionality. A new FileSorter class has been implemented, which encapsulates all the sorting functionality and provides a cleaner, more organized code structure.
    
    Major changes:
    - Converted script into an object-oriented design for better code organization and scalability. This design simplifies the sorting process into methods within the FileSorter class.
    - Added error handling to gracefully handle cases where the directory argument is not provided.
    - Files and folders are now classified into loose and bound, allowing for a more refined sorting process.
    - Created dedicated methods for moving files and folders to their respective directories, reducing code duplication.
    - Implemented command-line argument parsing using argparse module, providing a more user-friendly command-line interface.
    - Added docstrings and comments to enhance code readability and maintainability.

--- main.py
+++ main.py
@@ -1,40 +1,97 @@
-# Import os and sys modules to work with files, directories and command line arguments
 import os
-import sys
+import shutil
+from pathlib import Path
+import argparse
 
-# Check if a command line argument is given
-if len(sys.argv) > 1:
-    # Get the folder path from the first argument
-    folder_path = sys.argv[1]
-else:
-    # If no argument is given, print an error message and exit
-    print("Please provide a folder path as an argument.")
-    sys.exit()
 
-# Loop through all the items in the folder
-for item in os.listdir(folder_path):
-    # Get the full path of the item
-    item_path = os.path.join(folder_path, item)
-    # Check if the item is a file or a directory
-    if os.path.isfile(item_path):
-        # Get the file extension of the item
-        file_ext = os.path.splitext(item)[1]
-        # Create a new folder name based on the file extension
-        new_folder = file_ext[1:].upper() + " Files"
-        # Create a new folder path based on the folder name
-        new_folder_path = os.path.join(folder_path, new_folder)
-        # Check if the new folder already exists, if not create it
-        if not os.path.exists(new_folder_path):
-            os.mkdir(new_folder_path)
-        # Move the file to the new folder
-        os.rename(item_path, os.path.join(new_folder_path, item))
-    elif os.path.isdir(item_path):
-        # Create a new folder name for folders
-        new_folder = "Folders"
-        # Create a new folder path based on the folder name
-        new_folder_path = os.path.join(folder_path, new_folder)
-        # Check if the new folder already exists, if not create it
-        if not os.path.exists(new_folder_path):
-            os.mkdir(new_folder_path)
-        # Move the directory to the new folder
-        os.rename(item_path, os.path.join(new_folder_path, item))
+class FileSorter:
+    """
+    Class that sorts files in a specified directory based on their file extensions.
+    It moves the files to their respective folders named after their file extensions.
+    """
+
+    def __init__(self, directory):
+        """
+        Initialize the FileSorter with the target directory.
+        """
+        self.directory = Path(directory)
+        self.folders_folder = self.directory / 'Folders'
+        self.file_folders = {}
+
+        self.classify_files_and_folders()
+
+    def classify_files_and_folders(self):
+        """
+        Classify files and folders in the directory into loose and bound, based on their parent directories.
+        """
+        # Find all loose files and folders
+        loose_files = [file for file in self.directory.glob('*') if file.is_file()]
+        loose_folders = [folder for folder in self.directory.glob('*') if
+                         folder.is_dir() and folder != self.folders_folder]
+
+        # Create a mapping from file extension to destination folder
+        self.file_folders = {file.suffix[1:].upper(): self.directory / f'{file.suffix[1:].upper()} Files' for file in
+                             loose_files}
+
+        # Classify loose and bound files
+        self.loose_files = {file for file in loose_files if file.suffix[1:].upper() in self.file_folders}
+        self.bound_files = {file: file.parent for file in self.directory.rglob('*.*') if
+                            file.is_file() and file.parent != self.directory}
+
+        # Save loose folders
+        self.loose_folders = loose_folders
+
+    def resolve_duplicate_path(self, path):
+        """
+        Modify the path to ensure it does not duplicate an existing one.
+        """
+        if not path.exists():
+            return path
+
+        base_path = path.parent / path.stem
+        extension = path.suffix
+        counter = 1
+
+        while path.exists():
+            path = Path(str(base_path) + f"_{counter}" + extension)
+            counter += 1
+
+        return path
+
+    def move_files(self):
+        """
+        Move the loose files to their respective folders.
+        """
+        for file in self.loose_files:
+            classification = file.suffix[1:].upper()
+            destination_folder = self.file_folders[classification]
+            destination_folder.mkdir(exist_ok=True)
+            destination = self.resolve_duplicate_path(destination_folder / file.name)
+            shutil.move(str(file), str(destination))
+
+    def move_folders(self):
+        """
+        Move the loose folders to a central 'Folders' folder.
+        """
+        for folder in self.loose_folders:
+            if folder == self.folders_folder:
+                continue
+            destination = self.resolve_duplicate_path(self.folders_folder / folder.name)
+            shutil.move(str(folder), str(destination))
+
+    def sort(self):
+        """
+        The main function that sorts both files and folders.
+        """
+        self.move_files()
+        self.move_folders()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Sort files in a specified directory into folders by file type.")
+    parser.add_argument('directory', type=str, help="The target directory to sort.")
+
+    args = parser.parse_args()
+
+    sorter = FileSorter(args.directory)
+    sorter.sort()
